0883ff1a625c48318fdeef2050197f7969a5ffb7
Tên của bạn <huynh131204@gmail.com>
2025-11-01 00:34:11 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
